790612e89a92405245b0daf9ee9e2346f63dee00
Maxwell Long <brothergoos@gmail.com>
2020-01-21 08:55:27 -0700

    add user message prompt

---
+++ MoreOperators01-21-20/Module1.vb
@@ -0,0 +1,20 @@
+﻿Option Explicit On
+Option Strict On
+
+Module MoreOperators
+
+    Sub Main()
+        'prompt user for a message
+        'then display the message
+        Dim userMessage As String
+
+        'userMessage = "Yo"
+
+        Console.WriteLine("Enter a message. Press enter when done.")
+        userMessage = Console.ReadLine()
+        Console.WriteLine("You entered " & userMessage & " Nailed It!")
+        Console.ReadLine()
+
+    End Sub
+
+End Module
