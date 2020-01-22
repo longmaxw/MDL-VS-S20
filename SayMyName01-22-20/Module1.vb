82059cbbf6aec925f7f94aa66aa38765f51b7dd1
Maxwell Long <brothergoos@gmail.com>
2020-01-22 08:37:29 -0700

    Uploading say my name

---
+++ SayMyName01-22-20/Module1.vb
@@ -0,0 +1,19 @@
+﻿Option Explicit On
+Option Strict On
+
+Module SayMyName
+
+    Sub Main()
+
+        Dim userName As String
+
+        'userMessage = their name
+
+        Console.WriteLine("Please type your name and press Enter.")
+        userName = Console.ReadLine()
+        Console.WriteLine("Hello, " & userName & "! Welcome to our programming class.")
+        Console.ReadLine()
+
+    End Sub
+
+End Module
