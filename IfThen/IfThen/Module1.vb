50461cbbdbb94e1104ccb27b9aa7376bbc802ef2
Maxwell Long <brothergoos@gmail.com>
2020-01-23 08:54:15 -0700

    Make code inform user of more than one statement about two integers.

--- IfThen/IfThen/Module1.vb
+++ IfThen/IfThen/Module1.vb
@@ -2,11 +2,31 @@
 
     Sub Main()
 
-        If 1 > 2 Then
-            Console.WriteLine("True")
-        Else
-            Console.WriteLine("False")
+        Dim firstNumber As Integer
+        Dim secondNumber As Integer
+
+        firstNumber = 5
+        secondNumber = 3
+
+        If firstNumber > secondNumber Then
+            Console.WriteLine("" & firstNumber & " is greater than " & secondNumber & ".")
+        ElseIf firstNumber < secondNumber Then
+            Console.WriteLine("" & firstNumber & " is less than " & secondNumber & ".")
+        ElseIf firstNumber = secondNumber Then
+            Console.WriteLine("" & firstNumber & " is equal to " & secondNumber & ".")
         End If
+
+
+        If firstNumber <= secondNumber Then
+            Console.WriteLine("" & firstNumber & " is less or equal to than " & secondNumber & ".")
+        ElseIf firstNumber >= secondNumber Then
+            Console.WriteLine("" & firstNumber & " is greater than or equal to " & secondNumber & ".")
+        ElseIf firstNumber <> secondNumber Then
+            Console.WriteLine("" & firstNumber & " is not equal to " & secondNumber & ".")
+        Else
+            Console.WriteLine("Some other thing???")
+        End If
+        Console.ReadLine()
 
     End Sub
 
