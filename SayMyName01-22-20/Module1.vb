da4951ae9f435500d03bb8f58cf012991c352281
Maxwell Long <brothergoos@gmail.com>
2020-01-23 08:22:50 -0700

    Create IfThen statement

--- SayMyName01-22-20/Module1.vb
+++ SayMyName01-22-20/Module1.vb
@@ -1,17 +1,30 @@
-﻿Option Explicit On
+﻿'Maxwell Long
+'RCET0265
+'Spring 2020
+'SayMyName
+
+Option Explicit On
 Option Strict On
 
 Module SayMyName
 
     Sub Main()
 
+        'putting the userName in string format
         Dim userName As String
 
-        'userMessage = their name
+        'userName = their name
 
+        'prompts user for name
         Console.WriteLine("Please type your name and press Enter.")
+
+        'console reads the users response
         userName = Console.ReadLine()
+
+        'console greets user with name
         Console.WriteLine("Hello, " & userName & "! Welcome to our programming class.")
+
+        'giving the user time to read console message
         Console.ReadLine()
 
     End Sub
