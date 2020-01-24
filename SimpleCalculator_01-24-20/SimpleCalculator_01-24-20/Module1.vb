9bdbaffcc03b08c5f7d2324ee1d7f2e40ea8bc68
Maxwell Long <brothergoos@gmail.com>
2020-01-24 09:01:15 -0700

    Simple calculator calculates sum of two integers.

---
+++ SimpleCalculator_01-24-20/SimpleCalculator_01-24-20/Module1.vb
@@ -0,0 +1,24 @@
+﻿'Maxwell Long
+'RCET0265
+'01-24-20
+'SimpleCalculator
+
+Module Module1
+
+    Sub Main()
+
+        Dim firstNumber As String
+        Dim secondNumber As String
+
+        Console.WriteLine("Welcome to the simple calculator. Please enter the first number, press enter then the second number.")
+
+        firstNumber = Console.ReadLine
+        secondNumber = Console.ReadLine
+
+        Console.WriteLine(CInt(firstNumber) + CInt(secondNumber))
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
