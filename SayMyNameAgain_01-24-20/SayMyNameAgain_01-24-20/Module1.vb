fac81dd5e55f645dadc3d19d72524f4f6a8f3ef4
Maxwell Long <brothergoos@gmail.com>
2020-01-24 08:37:18 -0700

    Coordinate console responses with username inputs.

---
+++ SayMyNameAgain_01-24-20/SayMyNameAgain_01-24-20/Module1.vb
@@ -0,0 +1,31 @@
+﻿'Maxwell Long
+'RCET0265
+'Spring 2020
+'SayMyNameAgain
+
+Option Explicit On
+Option Strict On
+Option Compare Text
+
+Module Module1
+
+    Sub Main()
+
+        Dim userName As String
+
+        Console.WriteLine("Please enter your name. Then press enter.")
+        userName = Console.ReadLine()
+
+        If userName = "Emily" Then
+            Console.WriteLine("Welcome Emily!")
+        ElseIf userName = "Joe" Then
+            Console.WriteLine("Welcome Joe!")
+        ElseIf userName = "Maxwell" Then
+            Console.WriteLine("What's up dude.")
+        Else
+            Console.WriteLine("The name " & userName & " was not recognized. Access denied.")
+        End If
+        Console.ReadLine()
+    End Sub
+
+End Module
---
+++ SayMyNameAgain_01-24-20/SayMyNameAgain_01-24-20/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("SayMyNameAgain_01-24-20")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP Inc.")>
+<Assembly: AssemblyProduct("SayMyNameAgain_01-24-20")>
+<Assembly: AssemblyCopyright("Copyright © HP Inc. 2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("2c08f7fd-9037-434a-8998-7c33d6d9ab38")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
