bdf3faa222439243fcb0297052fc15917b228522
Maxwell Long <brothergoos@gmail.com>
2020-01-27 08:48:33 -0700

    user chooses two numbers and a function, recieves a result

--- SimpleCalculator_01-24-20/SimpleCalculator_01-24-20/Module1.vb
+++ SimpleCalculator_01-24-20/SimpleCalculator_01-24-20/Module1.vb
@@ -9,16 +9,36 @@ Module Module1
 
         Dim firstNumber As String
         Dim secondNumber As String
+        Dim productSum As String
 
-        Console.WriteLine("Welcome to the simple calculator. Please enter the first number, press enter then the second number.")
+        'greet user and prompt for first number
+        Console.WriteLine("Welcome to the simple calculator. Please enter the first number and press enter.")
+        firstNumber = Console.ReadLine()
 
-        firstNumber = Console.ReadLine
-        secondNumber = Console.ReadLine
+        'prompt user for second number
+        Console.WriteLine("Enter a second number and press enter.")
+        secondNumber = Console.ReadLine()
 
-        Console.WriteLine(CInt(firstNumber) + CInt(secondNumber))
+        'ask user to decide calculator function
+        Console.WriteLine("Would you like the product or the sum? Please type one option or the other. Press enter.")
+        productSum = Console.ReadLine()
 
+        'if user types "sum" the console uses addition
+        If productSum = "sum" Then
+            Console.WriteLine(CInt(firstNumber) + CInt(secondNumber))
+
+            'if user types "product the console uses multiplication
+        ElseIf productSum = "product" Then
+            Console.WriteLine(CInt(firstNumber) * CInt(secondNumber))
+
+            'display results
+            Console.ReadLine()
+
+        End If
+
+        'prompt user to finish and close window
+        Console.WriteLine("Press enter to finish. Thank you for using the Simple Calculator.")
         Console.ReadLine()
-
     End Sub
 
 End Module
