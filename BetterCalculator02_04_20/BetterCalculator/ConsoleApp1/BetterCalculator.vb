792fab5e08091c1509924569245d66a2da9e3cdb
Maxwell Long <brothergoos@gmail.com>
2020-02-12 08:39:00 -0700

    added smiley face

--- BetterCalculator02_04_20/BetterCalculator/ConsoleApp1/BetterCalculator.vb
+++ BetterCalculator02_04_20/BetterCalculator/ConsoleApp1/BetterCalculator.vb
@@ -3,15 +3,65 @@
 '02-04-20
 'BetterCalculator
 
-Module Module1
+Option Explicit On
+Option Strict On
+Option Compare On
+
+Module BetterCalculator
 
     Sub Main()
 
-        Dim myNumber As Integer
-        'Dim showPattern As String
-        'Dim doMath As String
+        Dim firstNumber As Integer
+        Dim secondNumber As Integer
+        Dim userChoice As String
+        Dim quitProgram As Boolean
+        Dim promptUser As Boolean
 
-        myNumber = 7
+        promptUser = True
+        quitProgram = False
+
+        Do While quitProgram = Fale
+            'prompt user for two numbers
+
+            Console.WriteLine("Please enter a number..")
+            Do While promptUser = True
+                Try
+                    firstNumber = CInt(Console.ReadLine())
+                    promptUser = False
+                Catch ex As Exception
+                    Console.WriteLine("Please enter a whole number")
+                    promptUser = True
+                End Try
+            Loop
+
+            Console.WriteLine("Please enter a second number..")
+            Try
+                secondNumber = CInt(Console.ReadLine())
+            Catch ex As Exception
+                Console.WriteLine("Please enter a whole number")
+            End Try
+
+            'prompt user for sum or product choice
+
+            'perform proper operation
+            Console.WriteLine("Please Choose an Option")
+            Console.WriteLine("1. Add")
+            Console.WriteLine("2. Multiply")
+            userChoice = Console.ReadLine()
+
+            'detirmine user's choice
+            If userChoice = "1" Then
+                'try
+                Console.WriteLine(firstNumber + secondNumber)
+                Console.WriteLine(firstNumber + secondNumber)
+                'Catch ex As InvalidCastException
+                '    Console.WriteLine("Please enter a whole number")
+                'Catch ex As Exception
+                '    Console.WriteLine("You broke it")
+                'End Try
+            End If
+        Loop
+
         '  Console.WriteLine("In Sub Main")
         '  Console.WriteLine(myNumber)
         '  Console.ReadLine()
