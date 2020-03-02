4ec0512b7e3666549847c1bc4b51d78d5cdb9302
Maxwell Long <brothergoos@gmail.com>
2020-03-02 08:21:16 -0700

    Layout and Design

---
+++ ListBoxForm/ListBoxForm/Form1.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataListBox = New System.Windows.Forms.ListBox()
+        Me.FirstNameTextBox = New System.Windows.Forms.TextBox()
+        Me.LastNameTextBox = New System.Windows.Forms.TextBox()
+        Me.AddItemButton = New System.Windows.Forms.Button()
+        Me.ClearAllButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'DataListBox
+        '
+        Me.DataListBox.FormattingEnabled = True
+        Me.DataListBox.Location = New System.Drawing.Point(12, 63)
+        Me.DataListBox.Name = "DataListBox"
+        Me.DataListBox.Size = New System.Drawing.Size(750, 251)
+        Me.DataListBox.TabIndex = 0
+        '
+        'FirstNameTextBox
+        '
+        Me.FirstNameTextBox.Font = New System.Drawing.Font("Russo One", 8.249999!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FirstNameTextBox.Location = New System.Drawing.Point(12, 37)
+        Me.FirstNameTextBox.Name = "FirstNameTextBox"
+        Me.FirstNameTextBox.Size = New System.Drawing.Size(359, 21)
+        Me.FirstNameTextBox.TabIndex = 1
+        Me.FirstNameTextBox.Text = "First Name Here"
+        '
+        'LastNameTextBox
+        '
+        Me.LastNameTextBox.Font = New System.Drawing.Font("Russo One", 8.249999!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LastNameTextBox.Location = New System.Drawing.Point(377, 37)
+        Me.LastNameTextBox.Name = "LastNameTextBox"
+        Me.LastNameTextBox.Size = New System.Drawing.Size(385, 21)
+        Me.LastNameTextBox.TabIndex = 2
+        Me.LastNameTextBox.Text = "Last Name Here"
+        '
+        'AddItemButton
+        '
+        Me.AddItemButton.Location = New System.Drawing.Point(12, 320)
+        Me.AddItemButton.Name = "AddItemButton"
+        Me.AddItemButton.Size = New System.Drawing.Size(249, 23)
+        Me.AddItemButton.TabIndex = 3
+        Me.AddItemButton.Text = "Add Item"
+        Me.AddItemButton.UseVisualStyleBackColor = True
+        '
+        'ClearAllButton
+        '
+        Me.ClearAllButton.Location = New System.Drawing.Point(267, 320)
+        Me.ClearAllButton.Name = "ClearAllButton"
+        Me.ClearAllButton.Size = New System.Drawing.Size(248, 23)
+        Me.ClearAllButton.TabIndex = 4
+        Me.ClearAllButton.Text = "Clear All"
+        Me.ClearAllButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(521, 320)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(241, 23)
+        Me.ExitButton.TabIndex = 5
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.ClearAllButton)
+        Me.Controls.Add(Me.AddItemButton)
+        Me.Controls.Add(Me.LastNameTextBox)
+        Me.Controls.Add(Me.FirstNameTextBox)
+        Me.Controls.Add(Me.DataListBox)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DataListBox As ListBox
+    Friend WithEvents FirstNameTextBox As TextBox
+    Friend WithEvents LastNameTextBox As TextBox
+    Friend WithEvents AddItemButton As Button
+    Friend WithEvents ClearAllButton As Button
+    Friend WithEvents ExitButton As Button
+End Class
---
+++ ListBoxForm/ListBoxForm/Form1.vb
@@ -0,0 +1,5 @@
+﻿Public Class Form1
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles LastNameTextBox.TextChanged
+
+    End Sub
+End Class
