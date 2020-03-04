99a14f6da6d6fa2e4791004e7c62e4091b41a6e3
Maxwell Long <brothergoos@gmail.com>
2020-03-04 14:37:31 -0700

    customized form, added menu item descriptions, added button function display when button is hovered over

---
+++ DinerMenu02-24-20/DinerMenu/DinerMenu/DinerMenuForm.Designer.vb
@@ -0,0 +1,124 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Maxs_Pub_Form
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.SoupButton = New System.Windows.Forms.Button()
+        Me.DinerNameLabel = New System.Windows.Forms.Label()
+        Me.DisplaySpecialLabel = New System.Windows.Forms.Label()
+        Me.SaladButton = New System.Windows.Forms.Button()
+        Me.FishButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.SuspendLayout()
+        '
+        'SoupButton
+        '
+        Me.SoupButton.Font = New System.Drawing.Font("Sigmar One", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(186, Byte))
+        Me.SoupButton.Location = New System.Drawing.Point(129, 374)
+        Me.SoupButton.Name = "SoupButton"
+        Me.SoupButton.Size = New System.Drawing.Size(130, 41)
+        Me.SoupButton.TabIndex = 0
+        Me.SoupButton.Text = "&Soup"
+        Me.ToolTip1.SetToolTip(Me.SoupButton, "dispay's soup of the day")
+        Me.SoupButton.UseVisualStyleBackColor = True
+        '
+        'DinerNameLabel
+        '
+        Me.DinerNameLabel.AutoSize = True
+        Me.DinerNameLabel.Font = New System.Drawing.Font("Sigmar One", 36.0!, CType((System.Drawing.FontStyle.Italic Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(186, Byte))
+        Me.DinerNameLabel.Location = New System.Drawing.Point(226, 9)
+        Me.DinerNameLabel.Name = "DinerNameLabel"
+        Me.DinerNameLabel.Size = New System.Drawing.Size(334, 79)
+        Me.DinerNameLabel.TabIndex = 4
+        Me.DinerNameLabel.Text = "Max's Pub"
+        '
+        'DisplaySpecialLabel
+        '
+        Me.DisplaySpecialLabel.Location = New System.Drawing.Point(12, 88)
+        Me.DisplaySpecialLabel.Name = "DisplaySpecialLabel"
+        Me.DisplaySpecialLabel.Size = New System.Drawing.Size(776, 283)
+        Me.DisplaySpecialLabel.TabIndex = 5
+        '
+        'SaladButton
+        '
+        Me.SaladButton.Font = New System.Drawing.Font("Sigmar One", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(186, Byte))
+        Me.SaladButton.Location = New System.Drawing.Point(265, 374)
+        Me.SaladButton.Name = "SaladButton"
+        Me.SaladButton.Size = New System.Drawing.Size(130, 41)
+        Me.SaladButton.TabIndex = 6
+        Me.SaladButton.Text = "S&alad"
+        Me.ToolTip1.SetToolTip(Me.SaladButton, "display's available salads for the day")
+        Me.SaladButton.UseVisualStyleBackColor = True
+        '
+        'FishButton
+        '
+        Me.FishButton.Font = New System.Drawing.Font("Sigmar One", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(186, Byte))
+        Me.FishButton.Location = New System.Drawing.Point(401, 374)
+        Me.FishButton.Name = "FishButton"
+        Me.FishButton.Size = New System.Drawing.Size(130, 41)
+        Me.FishButton.TabIndex = 7
+        Me.FishButton.Text = "&Fish"
+        Me.ToolTip1.SetToolTip(Me.FishButton, "display's available fish for the day")
+        Me.FishButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.ExitButton.Font = New System.Drawing.Font("Sigmar One", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(186, Byte))
+        Me.ExitButton.Location = New System.Drawing.Point(537, 374)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(130, 41)
+        Me.ExitButton.TabIndex = 8
+        Me.ExitButton.Text = "E&xit"
+        Me.ToolTip1.SetToolTip(Me.ExitButton, "exit menu")
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'Maxs_Pub_Form
+        '
+        Me.AcceptButton = Me.FishButton
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.ExitButton
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.FishButton)
+        Me.Controls.Add(Me.SaladButton)
+        Me.Controls.Add(Me.DisplaySpecialLabel)
+        Me.Controls.Add(Me.DinerNameLabel)
+        Me.Controls.Add(Me.SoupButton)
+        Me.Name = "Maxs_Pub_Form"
+        Me.Text = "Maxs_Pub_Special_Menu"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents SoupButton As Button
+    Friend WithEvents DinerNameLabel As Label
+    Friend WithEvents DisplaySpecialLabel As Label
+    Friend WithEvents SaladButton As Button
+    Friend WithEvents FishButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents ToolTip1 As ToolTip
+End Class
---
+++ DinerMenu02-24-20/DinerMenu/DinerMenu/DinerMenuForm.vb
@@ -0,0 +1,41 @@
+﻿'Maxwell Long
+'RCET0265
+'Spring 2020
+'Diner Menu
+
+Option Strict On
+Option Explicit On
+
+
+Public Class Maxs_Pub_Form
+
+    Private Sub SoupButton_Click(sender As Object, e As EventArgs) Handles SoupButton.Click
+        'type soupmessage
+        Dim soupMessage As String
+        soupMessage &= "~Soup of the Day~" & vbNewLine & "Beer Soup. Made with " _
+            & "choice of draft beer, fried flour, And locally grown potatoes."
+        DisplaySpecialLabel.Text = soupMessage
+    End Sub
+
+    Private Sub SaladButton_Click(sender As Object, e As EventArgs) Handles SaladButton.Click
+        'type saladmessage
+        Dim saladMessage As String
+        saladMessage &= "Ultimate Caesar Salad" & vbNewLine & "This really Is " _
+            & "the best homemade Caesar salad! With romaine lettuce, " _
+            & "Parmesan cheese,homemade croutons And a creamy homemade dressing."
+        DisplaySpecialLabel.Text = saladMessage
+    End Sub
+
+    Private Sub FishButton_Click(sender As Object, e As EventArgs) Handles FishButton.Click
+        'type fishmessage
+        Dim fishMessage As String
+        fishMessage &= "Greasy Salmon" & vbNewLine & "Locally caught, GREASY" _
+            & "slab of salmon on a plate."
+        DisplaySpecialLabel.Text = fishMessage
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+End Class
