d15e84b37075c2ac4de9cd5c8d18110805ab928c
Maxwell Long <brothergoos@gmail.com>
2020-03-04 08:40:14 -0700

    started project

---
+++ ArrayExample/ArrayExample/ArrayExample.vb
@@ -0,0 +1,29 @@
+﻿Module ArrayExample
+
+    Sub Main()
+        Dim myArray(3) As Integer
+
+        myArray(1) = 5
+
+        myArray = {1, 2, 3, 4}
+
+        Console.ReadLine()
+    End Sub
+    Private Sub InfoArrayExample()
+        Dim infoArray() As String
+
+        infoArray = Split("Yo,what up,Crunchwrap,fire sauce", ",")
+
+        For Each thingy In infoArray
+            Console.WriteLine(thingy)
+            infoArray.Contains(thingy)
+        Next
+
+        For i = LBound(infoArray) To UBound(infoArray)
+            Console.WriteLine("at" & Str(i) & " Value is: " & infoArray(i))
+        Next
+
+        Console.ReadLine()
+    End Sub
+
+End Module
---
+++ SubFunctionExample/SubFunctionExample/Module1.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim someNumbInMain As Integer
+        someNumbInMain
+
+    End Sub
+    Sub MySub(ByVal myNumberInMySub As Integer)
+        'Dim myNumber As Integer
+        'myNumber = myNumber + 2
+        myNumberInMySub += 2
+        Console.WriteLine("In Sub MySub")
+        Console.WriteLine(myNumberInMySub)
+        Console.ReadLine()
+    End Sub
+
+    Sub MySub(ByVal myNumberInMySub As Integer)
+
+    End Sub
+End Module
