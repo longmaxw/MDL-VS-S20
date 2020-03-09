862ae8f425afc5b9e62f497ffb488c1c7b23e451
Maxwell Long <brothergoos@gmail.com>
2020-03-09 09:08:51 -0600

    random numbers genrerated on two die

---
+++ RollTheDice/RollTheDice/Module1.vb
@@ -0,0 +1,40 @@
+﻿Module Module1
+
+
+
+    Private Function getFirstRandomIntegerOneToSix() As Integer
+
+        Dim maxSingle As Single : maxSingle = 5
+        Dim minSingle As Single : minSingle = 1
+        Dim tempSingle As Single
+        Randomize(System.DateTime.Now.Millisecond) 'set seed to current system millisecond
+        tempSingle = ((maxSingle - minSingle + 1) * Rnd() + minSingle) 'actually get the random number in the range
+        getFirstRandomIntegerOneToSix = Convert.ToInt32(tempSingle) 'return the number
+
+        Console.ReadLine()
+
+    End Function
+
+    Private Function getSecondRandomIntegerOneToSix() As Integer
+
+        Dim maxSingle As Single : maxSingle = 5
+        Dim minSingle As Single : minSingle = 1
+        Dim tempSingle As Single
+        Randomize(System.DateTime.Now.Millisecond) 'set seed to current system millisecond
+        tempSingle = ((maxSingle - minSingle + 1) * Rnd() + minSingle) 'actually get the random number in the range
+        getSecondRandomIntegerOneToSix = Convert.ToInt32(tempSingle) 'return the number
+
+        Console.WriteLine(getSecondRandomIntegerOneToSix)
+        Console.ReadLine()
+
+    End Function
+    Sub Main()
+
+        Console.WriteLine(getFirstRandomIntegerOneToSix() + getSecondRandomIntegerOneToSix())
+        Console.ReadLine()
+
+
+
+    End Sub
+
+End Module
