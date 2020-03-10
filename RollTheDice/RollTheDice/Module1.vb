e2011bf65521d4d8f76cff38922d5241445c9e41
Maxwell Long <brothergoos@gmail.com>
2020-03-10 09:09:56 -0600

    Console now adds the two dice and displays the value with labels on each value

--- RollTheDice/RollTheDice/Module1.vb
+++ RollTheDice/RollTheDice/Module1.vb
@@ -1,39 +1,50 @@
 ﻿Module Module1
 
 
+    Dim DiceSum As Single
 
-    Private Function getFirstRandomIntegerOneToSix() As Integer
+    Private Function DiceOne() As Integer
 
         Dim maxSingle As Single : maxSingle = 5
         Dim minSingle As Single : minSingle = 1
         Dim tempSingle As Single
         Randomize(System.DateTime.Now.Millisecond) 'set seed to current system millisecond
         tempSingle = ((maxSingle - minSingle + 1) * Rnd() + minSingle) 'actually get the random number in the range
-        getFirstRandomIntegerOneToSix = Convert.ToInt32(tempSingle) 'return the number
+        DiceOne = Convert.ToInt32(tempSingle) 'return the number
 
+        Console.WriteLine("Dice One =")
+        Console.WriteLine(DiceOne)
         Console.ReadLine()
 
     End Function
 
-    Private Function getSecondRandomIntegerOneToSix() As Integer
+    Private Function DiceTwo() As Integer
 
         Dim maxSingle As Single : maxSingle = 5
         Dim minSingle As Single : minSingle = 1
         Dim tempSingle As Single
         Randomize(System.DateTime.Now.Millisecond) 'set seed to current system millisecond
         tempSingle = ((maxSingle - minSingle + 1) * Rnd() + minSingle) 'actually get the random number in the range
-        getSecondRandomIntegerOneToSix = Convert.ToInt32(tempSingle) 'return the number
+        DiceTwo = Convert.ToInt32(tempSingle) 'return the number
 
-        Console.WriteLine(getSecondRandomIntegerOneToSix)
+
+        Console.WriteLine("Dice Two =")
+        Console.WriteLine(DiceTwo)
+        Console.ReadLine()
+
+    End Function
+    Private Function DiceTotal() As Integer
+
+        DiceSum = DiceOne() + DiceTwo()
+        Console.WriteLine("Dice One + Dice Two = " & DiceSum)
         Console.ReadLine()
 
     End Function
     Sub Main()
 
-        Console.WriteLine(getFirstRandomIntegerOneToSix() + getSecondRandomIntegerOneToSix())
-        Console.ReadLine()
+        Console.WriteLine(DiceTotal)
 
-
+        Dim Matrix(11,1) as Double
 
     End Sub
 
